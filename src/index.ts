26c32d14bf3296ca5f41f054c282650b97d1c97d
Sara Sallam <sara.sallam@breadfast.com>
2024-08-03 18:17:17 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello");
+import {createConnection } from "typeorm";
+import { Client } from "./entities/Client";
+import { Role } from "./entities/Role";
+import express from "express";
+import { createClientRouter } from "./routes/create_user";
+import { createRoleRouter } from "./routes/create_role";
+import { deleteClientRouter } from "./routes/delete_client";
+import { fetchUsersRouter } from "./routes/fetch_users";
+
+const app = express();
+const main = async() =>{
+    try{
+    const connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "",
+        database: "bfmovies",
+        entities: [Client, Role],
+        synchronize: true
+       
+
+    })
+    app.use(express.json());
+    app.use(createClientRouter);
+    app.use(createRoleRouter);
+    app.use(deleteClientRouter);
+    app.use(fetchUsersRouter);
+    app.listen(3000, ()=>{
+    });
+}catch(error){
+}
+}
+
+main();
