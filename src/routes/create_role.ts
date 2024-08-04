f1bf21f99736862ff0b87d1757a9b667ff911878
sarahhh12 <142973581+sarahhh122@users.noreply.github.com>
2024-08-04 11:43:35 +0300

    removed js files and only kept typescript files

--- src/routes/create_role.ts
+++ src/routes/create_role.ts
@@ -1,7 +1,7 @@
 import express from "express"
-import { Role, RoleTypes } from "../entities/Role";
 import { Client } from "../entities/Client";
-
+import { Role } from "../entities/Role";
+import { RoleTypes } from "../entities/Role";
 const router = express.Router();
 
 router.post("/api/client/:user_id/role", async(req,res)=>{
